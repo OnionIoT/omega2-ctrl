4a8cb6c76b7f5dca3b9db72be69343ce2a12cec3
greenbreakfast <lazardemin@gmail.com>
2016-07-29 19:43:10 -0400

    separated gpiomux functions into their own file

--- include/gpiomux.h
+++ include/gpiomux.h
@@ -1,18 +1,24 @@
-#ifndef _OMEGA2_CTRL_H_
-#define _OMEGA2_CTRL_H_
+#ifndef _GPIOMUX_H_
+#define _GPIOMUX_H_
 
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
 #include <omega2_ctrl_types.h>
-#include <gpiomux.h>
 
 
-#endif // _OMEGA2_CTRL_H_
+int 			gpiomux_set				(char *group, char *name);
+int 			gpiomux_get				(void);
+
+int 			gpiomux_mmap_open			(void);
+void 			gpiomux_mmap_close			(void);
+
+#endif // _GPIOMUX_H_
--- src/main.c
+++ src/main.c
@@ -1,121 +1,33 @@
 #include <main.h>
 
-
-
-
-static uint8_t* gpio_mmap_reg = NULL;
-static int gpio_mmap_fd = 0;
-
-static void __gpiomode_set(unsigned int mask, unsigned int shift, unsigned int val)
-{
-	volatile uint32_t *reg = (volatile uint32_t*) (gpio_mmap_reg + 0x60);
-	unsigned int v;
-
-	if (shift > 31) {
-		shift -= 32;
-		reg++;
-	}
-
-	v = *reg;
-	v &= ~(mask << shift);
-	v |= (val << shift);
-	*(volatile uint32_t*) (reg) = v;
-}
-
-static int gpiomode_set(char *group, char *name)
-{
-	int i;
-	int id;
-
-	for (id = 0; id < _O2_NUM_GPIO_MUX; id ++)
-		if (!strcmp(omega2GpioMux[id].name, group))
-			break;
-
-	if (id < _O2_NUM_GPIO_MUX) for (i = 0; i < 4; i++) {
-		if (!omega2GpioMux[id].func[i] || strcmp(omega2GpioMux[id].func[i], name))
-			continue;
-		__gpiomode_set(omega2GpioMux[id].mask, omega2GpioMux[id].shift, i);
-		fprintf(stderr, "set pinmux %s -> %s\n", omega2GpioMux[id].name, name);
-		return 0;
-	}
-	fprintf(stderr, "unknown group/function combination\n");
-	return -1;
-}
-
-static int gpiomode_get(void)
-{
-	unsigned int reg = *(volatile uint32_t*) (gpio_mmap_reg + 0x60);
-	unsigned int reg2 = *(volatile uint32_t*) (gpio_mmap_reg + 0x64);
-	int id;
-
-	for (id = 0; id < _O2_NUM_GPIO_MUX; id ++) {
-		unsigned int val;
-		int i;
-
-		if (omega2GpioMux[id].shift < 32)
-			val = (reg >> omega2GpioMux[id].shift) & omega2GpioMux[id].mask;
-		else
-			val = (reg2 >> (omega2GpioMux[id].shift - 32)) & omega2GpioMux[id].mask;
-
-		fprintf(stderr, "Group %s - ", omega2GpioMux[id].name);
-		for (i = 0; i < 4; i++) {
-			if (!omega2GpioMux[id].func[i])
-				continue;
-			if (i == val)
-				fprintf(stderr, "[%s] ", omega2GpioMux[id].func[i]);
-			else
-				fprintf(stderr, "%s ", omega2GpioMux[id].func[i]);
-		}
-		fprintf(stderr, "\n");
-	}
-
-	return 0;
-}
-
-static int gpiomode_mmap(void)
-{
-	if ((gpio_mmap_fd = open(MMAP_PATH, O_RDWR)) < 0) {
-		fprintf(stderr, "unable to open mmap file");
-		return -1;
-	}
-
-	gpio_mmap_reg = (uint8_t*) mmap(NULL, 1024, PROT_READ | PROT_WRITE,
-		MAP_FILE | MAP_SHARED, gpio_mmap_fd, 0x10000000);
-	if (gpio_mmap_reg == MAP_FAILED) {
-		perror("failed to mmap");
-		fprintf(stderr, "failed to mmap");
-		gpio_mmap_reg = NULL;
-		close(gpio_mmap_fd);
-		return -1;
-	}
-
-	return 0;
-}
-
 int main(int argc, char **argv)
 {
-	int ret = -1;
+	int status	= EXIT_FAILURE;
 
-	if (gpiomode_mmap())
-		return -1;
 
 	// read the command argument
 	if (argc >= 2) {
 		if (strcmp(argv[1], "gpiomux") == 0) {
+			if (gpiomux_mmap_open() == EXIT_FAILURE) {
+				return EXIT_FAILURE;
+			}
+
 			if (argc >= 5 && !strcmp(argv[2], "set")) {
-				ret = gpiomode_set(argv[3], argv[4]);
+				status = gpiomux_set(argv[3], argv[4]);
 			} 
 			else if (argc >= 3 && !strcmp(argv[2], "get")) {
-				ret = gpiomode_get();
+				status = gpiomux_get();
 			} 
 			else {
 				fprintf(stderr, "%s set <group> <function>\n", *argv);
 				fprintf(stderr, "%s get\n", *argv);
 			}
+
+			gpiomux_mmap_close();		
 		}
 	}
 
-	close(gpio_mmap_fd);
+	
 
-	return ret;
+	return status;
 }
